468cf97f964cdc1e18fe3a4b6a183239a24e423b
Hitendra Kumar <kuarhitendra635@gmail.com>
2026-05-18 12:32:40 +0530

    fixed signup api url

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,17 +2,14 @@ import { useState } from "react";
 import axios from "axios";
 
 function Signup() {
-
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const signupUser = async () => {
-
     try {
-
       await axios.post(
-        "team-task-manager-production-6f19.up.railway.app/api/auth/signup",
+        "https://team-task-manager-production-6f19.up.railway.app/api/auth/signup",
         {
           name,
           email,
@@ -24,9 +21,9 @@ function Signup() {
       alert("Signup Success");
 
     } catch (error) {
+      console.log(error);
 
       alert("Signup Failed");
-
     }
   };
 
@@ -40,35 +37,27 @@ function Signup() {
         gap: "10px",
       }}
     >
-
       <h1>Signup</h1>
 
       <input
         placeholder="Enter Name"
-        onChange={(e) =>
-          setName(e.target.value)
-        }
+        onChange={(e) => setName(e.target.value)}
       />
 
       <input
         placeholder="Enter Email"
-        onChange={(e) =>
-          setEmail(e.target.value)
-        }
+        onChange={(e) => setEmail(e.target.value)}
       />
 
       <input
         type="password"
         placeholder="Enter Password"
-        onChange={(e) =>
-          setPassword(e.target.value)
-        }
+        onChange={(e) => setPassword(e.target.value)}
       />
 
       <button onClick={signupUser}>
         Signup
       </button>
-
     </div>
   );
 }
